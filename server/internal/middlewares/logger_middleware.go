397ab514d4ad720ce205460b73740749d5cc4463
HT-Tuan <huynhthanhtuan2206@gmail.com>
2025-07-26 23:01:03 +0700

    update write log for request and response

--- server/internal/middlewares/logger_middleware.go
+++ server/internal/middlewares/logger_middleware.go
@@ -1,13 +1,30 @@
 package middlewares
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 )
+
+type CustomResponseWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w *CustomResponseWriter) Write(data []byte) (n int, err error) {
+	w.body.Write(data)
+	return w.ResponseWriter.Write(data)
+}
 
 func LoggerMiddleware() gin.HandlerFunc {
 	logPath := "internal/logs/http.log"
@@ -26,11 +43,99 @@ func LoggerMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		start := time.Now()
 
+		contentType := ctx.GetHeader("Content-Type")
+		requestBody := make(map[string]any)
+		formFiles := []map[string]any{}
+
+		if strings.HasPrefix(contentType, "multipart/form-data") {
+			if err := ctx.Request.ParseMultipartForm(32 << 20); err == nil && ctx.Request.MultipartForm != nil {
+				// Text
+				for key, value := range ctx.Request.MultipartForm.Value {
+					if len(value) == 1 {
+						requestBody[key] = value[0]
+					} else {
+						requestBody[key] = value
+					}
+				}
+
+				// File
+				for field, files := range ctx.Request.MultipartForm.File {
+					for _, f := range files {
+						formFiles = append(formFiles, map[string]any{
+							"field":        field,
+							"filename":     f.Filename,
+							"size":         formatFileSize(f.Size),
+							"content_type": f.Header.Get("Content_Type"),
+						})
+					}
+				}
+				if len(formFiles) > 0 {
+					requestBody["form_files"] = formFiles
+				}
+			}
+
+			log.Println("multipart/form-data")
+		} else {
+			// Content-Type: application/json
+			// Content-Type: application/x-www-form-urlencoded
+
+			bodyBytes, err := io.ReadAll(ctx.Request.Body)
+
+			if err != nil {
+				logger.Err(err).Msg("Failed to read request body")
+			}
+
+			ctx.Request.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+			if strings.HasPrefix(contentType, "application/json") {
+				//application/json
+				_ = json.Unmarshal(bodyBytes, &requestBody)
+			} else {
+				// application/x-www-form-urlencoded NOTE: data been encoded
+				values, _ := url.ParseQuery(string(bodyBytes))
+				for key, value := range values {
+					if len(value) == 1 {
+						requestBody[key] = value[0]
+					} else {
+						requestBody[key] = value
+					}
+				}
+			}
+		}
+
+		customWriter := &CustomResponseWriter{
+			ResponseWriter: ctx.Writer,
+			body:           bytes.NewBufferString(""),
+		}
+
+		ctx.Writer = customWriter
+
+		ctx.Next()
+
 		logEvent := logger.Info()
 
 		duration := time.Since(start)
 
 		statusCode := ctx.Writer.Status()
+
+		responseContentType := ctx.Writer.Header().Get("Content-Type")
+		responseBodyRaw := customWriter.body.String()
+		var responseBodyParsed interface{}
+		if strings.HasPrefix(responseContentType, "image/") {
+			//image
+			responseBodyParsed = "[BINARY DATA]"
+		} else if strings.HasPrefix(responseContentType, "application/json") ||
+			strings.HasPrefix(strings.TrimSpace(responseBodyRaw), "{") ||
+			strings.HasPrefix(strings.TrimSpace(responseBodyRaw), "[") {
+			//json
+			if err := json.Unmarshal([]byte(responseBodyRaw), &responseBodyParsed); err != nil {
+				responseBodyParsed = responseBodyRaw
+			}
+		} else {
+			//text
+			responseBodyParsed = responseBodyRaw
+		}
+
 		if statusCode >= 500 {
 			logEvent = logger.Error()
 		} else if statusCode >= 400 {
@@ -50,9 +155,22 @@ func LoggerMiddleware() gin.HandlerFunc {
 			Str("request_uri", ctx.Request.RequestURI).
 			Int64("content_length", ctx.Request.ContentLength).
 			Interface("headers", ctx.Request.Header).
+			Interface("request_body", requestBody).
 			Int("status_code", statusCode).
+			Interface("response_body", responseBodyParsed).
 			Int64("duration_ms", duration.Microseconds()).
 			Msg("HTTP Request Log")
 
+	}
+}
+
+func formatFileSize(size int64) string {
+	switch {
+	case size >= 1<<20:
+		return fmt.Sprintf("%.2f MB", float64(size)/(1<<20))
+	case size >= 1<<10:
+		return fmt.Sprintf("%.2f KB", float64(size)/(1<<10))
+	default:
+		return fmt.Sprintf("%d B", size)
 	}
 }
